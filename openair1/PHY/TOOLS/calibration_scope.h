92e87c628423bf2eb6e4e36621b56ded12e1e8b4
laurent <laurent.thomas@open-cells.com>
2025-01-21 15:21:22 +0100

    first trial

--- openair1/PHY/TOOLS/calibration_scope.h
+++ openair1/PHY/TOOLS/calibration_scope.h
@@ -1,7 +1,17 @@
 #ifndef CALIB_SCOPE_H
 #define CALIB_SCOPE_H
-
+static const int DFT = 8 * 1024;
 #include "common_lib.h"
 
-void CalibrationInitScope(void **samplesRx, openair0_device_t *rfdevice);
+
+typedef struct {
+  openair0_device_t *rfdevice;
+  int antennas;
+  int dft_sz;
+  c16_t **samplesRx;
+  c16_t **samplesTx;
+  pthread_mutex_t rxMutex;
+  pthread_mutex_t txMutex;
+} threads_t;
+void CalibrationInitScope(threads_t *p);
 #endif
