04878efb18ac2ffe91e50235fdde12c2da5b376b
Laurent THOMAS <laurent.thomas@open-cells.com>
2026-02-17 16:41:11 +0100

    add command line options for calibration test tool

--- openair1/PHY/TOOLS/calibration_scope.h
+++ openair1/PHY/TOOLS/calibration_scope.h
@@ -1,10 +1,21 @@
 #ifndef CALIB_SCOPE_H
 #define CALIB_SCOPE_H
+
 static const int DFT = 8 * 1024;
 #include "common_lib.h"
 
+typedef struct {
+  uint tx;
+  uint rx;
+  uint freq;
+  uint chirp;
+  uint amplitude;
+  uint sinus_freq;
+  uint dft;
+} config_t;
 
 typedef struct {
+  config_t *c;
   openair0_device_t *rfdevice;
   int antennas;
   int dft_sz;
