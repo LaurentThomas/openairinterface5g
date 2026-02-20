cca91aa417f2ca878f649c310f54f99689c10039
Laurent <laurent.thomas@open-cells.com>
2026-02-20 16:24:20 +0100

    add qpsk modulation

--- openair1/PHY/TOOLS/calibration_scope.h
+++ openair1/PHY/TOOLS/calibration_scope.h
@@ -1,9 +1,7 @@
 #ifndef CALIB_SCOPE_H
 #define CALIB_SCOPE_H
 
-static const int DFT = 8 * 1024;
-#include "common_lib.h"
-enum e_pattern_scheme { e_CHIRP = 0, e_QAM_256, e_SINUS, e_MAX_PATT_SCHEME };
+enum e_pattern_scheme { e_SINUS, e_CHIRP, e_QPSK, e_QAM_16, e_QAM_256, e_MAX_PATT_SCHEME };
 
 typedef struct {
   uint tx;
